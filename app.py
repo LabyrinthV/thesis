f08d9c22b0cb4be279201deca047dc30dfb250d6
Osman <osman44.ali@gmail.com>
2024-08-11 20:56:12 +0300

    modified pickle file and made some changes to notebook

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import streamlit as st
-
+import gzip
 st.title('Hello, Streamlit!')
 st.write('This is a simple Streamlit app running in a Docker container.')
 
-# Import the classifiers from the dl_classifier.pkl file
 import pickle
-with open('dl_classifier.pkl', 'rb') as f:
-    dl_classifiers = pickle.load(f)
 
-# Import the ul classifiers from the ul_classifier.pkl file
-with open('ul_classifier.pkl', 'rb') as f:
-    ul_classifiers = pickle.load(f)
-
-with open('netflix_model.pkl', 'rb') as f:
+with open('./afreeca_model.pkl', 'rb') as f:
     netflix_model = pickle.load(f)
 
 # Create a function that takes in a model and a list of features and returns a prediction
@@ -20,7 +13,7 @@ def predict(model, features):
     return model.predict(features)
 
 # Bitrate
-bitrate = st.slider('Downlink Bitrate', 0, 3000000, 50000)
+bitrate = st.slider('Downlink Bitrate', 0, 500000, 50000)
 
 make_prediction = st.checkbox('Make Prediction')
 
@@ -29,9 +22,11 @@ if make_prediction:
     features = [[bitrate]]
 
     # Call the predict function with the model and the features
-    prediction = predict(netflix_model, features.reshape(1, -1))
+    prediction = predict(netflix_model, features)
     # If the prediction is 1 it is an inlier, if it is -1 it is an outlier
     if prediction == 1:
+        print(prediction)
         st.write(f'The predicted downlink throughput is predicted to be an inlier')
     else:
+        print(prediction)
         st.write(f'The predicted downlink throughput is predicted to be an outlier')
